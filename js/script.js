e5f6e24a82abe4570120ccc95f512eb857ec4f72
Adebimpe <realadebimpe@gmail.com>
2020-09-16 17:11:42 +0100

    modified css style

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// const input = document.querySelector('#input');
+// const inputError = document.getElementsByClassName('.input-error')
+// input.style.display ='none'
+
+// function handleSubmit(event){
+//     event.preventDefault;
+
+
+//     const input = event.target.elements.value;
+
+//     if(input !== "name"){
+//         return "invalid user"
+//         inputError.classList.add('error')
+//     error.style.display = ''
+//     }else{
+//         return "true"
+//         inputError.classList.remove('error')
+//         error.style.display = 'none'
+//     }
+
+// }
+// form.addEventListener('submit', handleSubmit )
